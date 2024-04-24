8e54dc7281c9991bdb73098ce04b853722da2ce3
Yannis Kyriakopoulos <66531087+YaniKyr@users.noreply.github.com>
2024-04-24 21:21:02 +0300

    Update Setupp.sh

--- Setupp.sh
+++ Setupp.sh
@@ -5,11 +5,13 @@ multipass launch --name Master2 -m 3G -d 20G -c 2
 
 multipass launch --name Worker -m 3G -d 20G -c 2
 
-
 multipass exec Master1 -- sudo snap install microk8s --classic
-multipass exec Master2 -- sudo snap install microk8s --classic  
+#multipass exec Master2 -- sudo snap install microk8s --classic  
 multipass exec Worker -- sudo snap install microk8s --classic
 
+./ClusterSetup.sh Worker Master1
 
-./ClusterLaunch.sh Worker Master1
-./ClusterLaunch.sh Master2 Master1
+#Configure Prometheus Init
+#Add NodePort
+#Set Microk8s Creditentials
+#Create Liveness Checks
