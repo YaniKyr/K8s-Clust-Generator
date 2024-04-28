4f5537d98d7d5ba02a2f71941a0b7abd6c2992d8
Yannis Kyriakopoulos <66531087+YaniKyr@users.noreply.github.com>
2024-04-28 03:23:08 +0300

    Add files via upload

--- IPro.sh
+++ IPro.sh
@@ -1,3 +1,7 @@
-multipass exec $1 -- helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-multipass exec $1 -- helm repo update
-multipass exec $1 -- helm install prometheus prometheus-community/kube-prometheus-stack
+#!/bin/bash
+
+multipass exec $1 -- microk8s helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+multipass exec $1 -- microk8s helm repo update
+multipass exec $1 -- microk8s helm install prometheus prometheus-community/kube-prometheus-stack
+multipass exec $1 -- microk8s helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+multipass exec $1 -- microk8s helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
--- Setupp.sh
+++ Setupp.sh
@@ -1,29 +1,24 @@
+#!/bin/bash
+function configureVms(){
+    multipass launch --name $1 -m 3G -d 20G -c 2
+    multipass exec $1 -- sudo snap install microk8s --classic
+    multipass exec $1 -- sudo usermod -a -G microk8s ubuntu
+    multipass exec $1 -- newgrp microk8s
+    multipass exec $1 -- rm -rf .bashrc
+    multipass transfer .bashrc $1:.bashrc
+}
 
-multipass launch --name Master1 -m 3G -d 20G -c 2
+configureVms Master1
+configureVms Master2
+configureVms Worker
+./joinNode.sh Worker Master1
+./joinNode.sh Master2 Master1
+./IPro.sh Master1
 
-multipass launch --name Master2 -m 3G -d 20G -c 2
 
-multipass launch --name Worker -m 3G -d 20G -c 2
-
-multipass exec Master1 -- sudo snap install microk8s --classic
-multipass exec Master2 -- sudo snap install microk8s --classic  
-multipass exec Worker -- sudo snap install microk8s --classic
-
-#./ClusterSetup.sh Worker Master1
-
-#Configure Prometheus Init
 #Add NodePort
 #Set Microk8s Creditentials
 #Create Liveness Checks
-#newgrp microk8s
-#   19  
-#   20  source ~/.bashrc 
-#      38  kubectl describe pod prometheus-prometheus-kube-prometheus-prometheus-0
-#   39  vi NodePPrometheus.yaml
-#   40  kubectl apply -f NodePPrometheus.yaml -n default
-#sed -i -e $'$a\\\ alias kubectl=\'microk8s kubectl\'' ~/.bashrc
-# source ~/.bashrc
-#
-#helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-#helm repo update
-#helm install prometheus prometheus-community/kube-prometheus-stack
+
+
+
