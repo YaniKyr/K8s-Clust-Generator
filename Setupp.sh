e5f5b01e4b3ccef6d5032f732a073493c44045cb
Yannis Kyriakopoulos <66531087+YaniKyr@users.noreply.github.com>
2024-05-13 23:33:33 +0300

    Add files via upload

--- Setupp.sh
+++ Setupp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function configureVms(){
     echo $1
-    multipass launch --name $1 -m 3G -d 20G -c 2 
+    multipass launch --name $1 -m 3G -d 12G -c 2 
     multipass exec $1 -- sudo snap install microk8s --classic
     multipass exec $1 -- sudo usermod -a -G microk8s ubuntu
     multipass restart $1
@@ -31,24 +31,26 @@ function addMaster(){
 function InitVms(){
     configureVms $2
     configureVms $3
-    configureVms $4
+   configureVms $4
 
-    if [ $1 = "-1M2W" ] ; then
+    if [ $1 = "-1M2W" ]; then
         addWorker $4 $2
         addWorker $3 $2
-    elif [ $1 = "-3M" ] ; then
+    elif [ $1 = "-3M" ]; then
         addMaster $4 $2
         addMaster $3 $2
-    elif [ $1 = "-2M1W" ] ; then
+    elif [ $1 = "-2M1W" ]; then
         addWorker $4 $2
         addMaster $3 $2
+    else
+	   echo 'Unknown Parameter' 
     fi
     
     Services $2     
     multipass transfer NodePort.yaml $2:NodePort.yaml
     multipass transfer AuthRBAC.yaml $2:AuthRBAC.yaml
-    multipass exec $2 -- kubectl apply -f NodePort.yaml 
-    multipass exec $2 -- kubectl apply -f AuthRBAC.yaml 
+    multipass exec $2 -- microk8s kubectl apply -f NodePort.yaml 
+    multipass exec $2 -- microk8s kubectl apply -f AuthRBAC.yaml 
 }
 
 InitVms $1 $2 $3 $4
